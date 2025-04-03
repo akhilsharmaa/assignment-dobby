18c0529e9eacee9b1e4668c3cfed1cee22357885
Akhilesh <akhilsharma3332@gmail.com>
2025-04-03 23:42:23 +0530

    [roots] fixed.

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -4,7 +4,7 @@ function navbar() {
     return ( 
         <nav className="bg-slate-600 text-white border-gray-200 ">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href=" " className="flex items-center space-x-3 rtl:space-x-reverse">
+                <a href="\" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="https://static.wixstatic.com/media/e1fa8a_974cde8be06a4f5294d7595a9e3ba2d2~mv2.png/v1/fill/w_109,h_27,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/e1fa8a_974cde8be06a4f5294d7595a9e3ba2d2~mv2.png" className="h-8" alt="Flowbite Logo" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap ">
                         (Assignment)
